a64752e2db4e2932e149fee552b5fd80a690e770
Kiran Kumar A <kirankumar.annam@gmail.com>
2016-01-28 17:32:38 +0530

    Prints start
    This script prints no of stars, usage : $sh print_start.sh 33

---
+++ Print_start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+x=$1
+
+for (( i=1; i <= $x; ++i ))
+do
+	for (( j=$x; j >= $i; j-- ))
+	do
+		echo -ne " "
+	done
+	for (( n=1; n <= $i; ++n ))
+	do
+		echo -ne "* "
+	done
+	echo -e "\n"
+
+done
+x=$((x -1 ))
+for (( i=1; i <= $x; ++i ))
+do
+	for (( j=1; j <= $i; j++ ))
+	do
+		echo -ne " "
+	done
+	for (( n=$x; n >= $i; n-- ))
+	do
+		echo -ne " *"
+	done
+	echo -e "\n"
+done
